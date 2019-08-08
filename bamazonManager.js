07338e07ce520ab6c34eec44f659254f3180bd05
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-08-07 20:28:14 -0400

    created getProductsLowerThanFive() function

--- bamazonManager.js
+++ bamazonManager.js
@@ -14,34 +14,48 @@ connection.connect(function(err) {
     console.log("\n-------------------------Welcome to BAMAZON-------------------------");
 });
 
-var mysql = require ("mysql");
-var inquirer = require ("inquirer");
-
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "JW55cw04",
-    database: "bamazon"
-});
-
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("\n-------------------------Welcome to BAMAZON-------------------------");
-});
 
 function askManager(){
     //ask the manager for input
     inquirer.prompt([
         {
             type: "list",
-            name: "selection",
+            name: "menu",
             message: "What would you like to do?",
             choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
 
         },
-    ]).then(function(input) {
-
+    ]).then(function(response) {
+        if(response.menu == "View Products for Sale"){
+            queryAllProducts();
+        }else if(response.menu == "View Low Inventory"){
+            getProductsLowerThanFive();
+        }
     });
 }
+
+function queryAllProducts() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      console.log("-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-xx-x-x-x-x-x-x-x-x-x-x-x-x-x");
+      for (var i = 0; i < res.length; i++) {
+        console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        console.log("--------------------------------------------------------------------");
+      }
+      console.log("-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-xx-x-x-x-x-x-x-x-x-x-x-x-x-x");
+    });
+  }
+
+  function getProductsLowerThanFive() {
+    connection.query("SELECT * FROM products WHERE stock_quantity < ?", [5], function(err, res) {
+      if (err) throw err;
+      console.log("-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-xx-x-x-x-x-x-x-x-x-x-x-x-x-x");
+      for (var i = 0; i < res.length; i++) {
+        console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        console.log("--------------------------------------------------------------------");
+      }
+      console.log("-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-xx-x-x-x-x-x-x-x-x-x-x-x-x-x");
+    });
+
+  }
 askManager();
