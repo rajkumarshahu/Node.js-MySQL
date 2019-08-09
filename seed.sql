769e29ceda723520f06e45518df5f16607a9e361
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-08-09 08:41:06 -0400

    create separate seed.sql file

---
+++ seed.sql
@@ -0,0 +1,13 @@
+USE bamazon;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("JavaScript: The Good Parts", "Books", 25.62, 137),
+  ("Shadow of the Tomb Raider", "Video Games", 29.99, 177),
+  ("CYBERPOWERPC Gamer Xtreme ", "Computers", 1099.99, 31),
+  ("Tea Tree Body & Foot Scrub", "Beauty Store", 14.99, 59),
+  ("LEGO Marvel Super Heroes Avengers", "Toys and Games", 31.97, 89),
+  ("Pullover Hoody Sweatshirt", "Fashion", 101.99, 92),
+  ("Nintendo Switch Console", "Video Games", 399.99, 33),
+  ("Dark Age", "Books", 52.91, 11),
+  ("Razer Blade 15 Gaming Laptop", "Computers", 2691.21, 3),
+  ("Gary Games GGS 001", "Toys and Games", 39.77, 55);
